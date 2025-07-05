87e6af200aa44a903322ea39d424e20d3afa67d0
ecnryptionStudios <calvin.heiniger@outlook.com>
2025-07-05 09:03:12 -0700

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,8 +1,14 @@
 namespace Rfid{
+    //%block="read last data from the rfid scanner"
     export function read(){
 
     }
+    //%block="write $to to the rfid"
     export function write(to: any){
 
+    }
+    //%block
+    export function onRfidTap(handler: ()=>{}){
+        
     }
 }
